f4969656d1798ba19dfb8eabbcc622463ee91355
Pavel Zwerschke <pavelzw@gmail.com>
2023-09-04 19:25:27 +0200

    remove

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is vercel.live;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app vercel.live
+  frame-src giscus.app
 `
 
 const securityHeaders = [
