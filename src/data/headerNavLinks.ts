db479aaf0f788a51df607482b91d5bc8251a2dee
Pavel Zwerschke <pavelzw@gmail.com>
2026-01-24 00:18:53 +0100

    wip

--- src/data/headerNavLinks.ts
+++ src/data/headerNavLinks.ts
@@ -1,6 +1,5 @@
 export const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/cv', title: 'CV' },
   { href: '/about', title: 'About' },
