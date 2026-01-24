e7b5e49870de74b2616e9622cae55bee66fd623d
Pavel Zwerschke <pavelzw@gmail.com>
2026-01-24 02:20:03 +0100

    wip

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,31 +1,38 @@
+import anvil from '../assets/projects/anvil.jpg';
+import pixiBanner from '../assets/projects/pixi-banner.png';
+import mamba from '../assets/projects/mamba.jpg';
+import sevenCircles from '../assets/projects/seven-circles.png';
+import tree from '../assets/projects/tree.jpg';
+import comics from '../assets/projects/comics.jpg';
+
 export const projectsData = [
   {
     title: 'conda-forge',
     description: `conda-forge is a community-led conda channel that provides
     packages for a wide range of software. I have contributed to the conda-forge
     community by maintaining over 50 packages.`,
-    imgSrc: '/static/images/anvil.jpg',
+    img: anvil,
     href: 'https://github.com/search?type=code&q=owner%3Aconda-forge+path%3Arecipe%2Fmeta.yaml+pavelzw',
   },
   {
     title: 'setup-pixi',
     description: `setup-pixi is a GitHub action that installs pixi, a new conda
     package manager, and creates a cached conda environment using pixi.`,
-    imgSrc: '/static/images/pixi-banner.png',
+    img: pixiBanner,
     href: 'https://github.com/prefix-dev/setup-pixi',
   },
   {
     title: 'setup-micromamba',
     description: `setup-micromamba is a GitHub action that installs micromamba and
     creates a conda environment.`,
-    imgSrc: '/static/images/mamba.jpg',
+    img: mamba,
     href: 'https://github.com/mamba-org/setup-micromamba',
   },
   {
     title: 'The Seven Circles Theorem',
     description: `As part of the Summer of Maths Exposition 2,
     I created a video explaining the Seven Circles Theorem. The video was created using manim.`,
-    imgSrc: '/static/images/seven-circles.png',
+    img: sevenCircles,
     href: 'https://youtu.be/m9v0h2ibYpo',
   },
   {
@@ -33,14 +40,14 @@ export const projectsData = [
     description: `slim-trees is a Python package for saving and loading
     compressed sklearn Tree-based and lightgbm models. The compression
     is performed by modifying how the model is pickled by Python's pickle module.`,
-    imgSrc: '/static/images/tree.jpg',
+    img: tree,
     href: '/blog/slim-trees',
   },
   {
     title: 'Calibre Kindle Comics',
     description: `calibre-kindle-comics is a calibre plugin that converts cbz and cbr files
     to mobi files that are optimized for Kindle devices.`,
-    imgSrc: '/static/images/comics.jpg',
+    img: comics,
     href: 'https://github.com/pavelzw/calibre-kindle-comics',
   },
 ];
