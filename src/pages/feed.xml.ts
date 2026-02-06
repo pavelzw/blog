25aa40c84399897ba4fb9ff09e47c218939fa07e
Pavel Zwerschke <pavelzw@gmail.com>
2026-02-07 00:17:59 +0100

    Add full posts in rss feed

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,27 +1,41 @@
-import { getCollection } from 'astro:content';
+import mdxRenderer from '@astrojs/mdx/server.js';
 import rss from '@astrojs/rss';
 import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { siteMetadata } from '../data/siteMetadata';
 
 export const prerender = true;
 
 export async function GET(context: APIContext) {
+  const container = await AstroContainer.create({
+    renderers: [{ name: 'astro:jsx', ssr: mdxRenderer }],
+  });
+
   const posts = (await getCollection('blog', ({ data }) => !data.draft)).sort(
     (a, b) => b.data.date.valueOf() - a.data.date.valueOf(),
   );
 
-  return rss({
-    title: siteMetadata.title,
-    description: siteMetadata.description,
-    site: context.site || siteMetadata.siteUrl,
-    items: posts.map((post) => ({
+  const items = [];
+  for (const post of posts) {
+    const { Content } = await post.render();
+    const content = await container.renderToString(Content);
+    items.push({
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.summary || '',
       link: `/blog/${post.slug}/`,
       categories: post.data.tags,
       author: `${siteMetadata.author.email} (${siteMetadata.author.name})`,
-    })),
+      content,
+    });
+  }
+
+  return rss({
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    site: context.site || siteMetadata.siteUrl,
+    items,
     customData: `<language>${siteMetadata.language}</language>`,
   });
 }
